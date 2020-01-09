d8b79f0180b5748a8f60b8c0d58f4758f1ce2d46
Robin <daorob@gmail.com>
2020-01-09 00:08:01 -0600

    Add why tuples notes.

--- tuples.ts
+++ tuples.ts
@@ -17,3 +17,10 @@ const pepsi: Drink = ['brown', true, 40];
 pepsi[0] = 40; // won't work because we are assigning a different type
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+    horsepower: 400,
+    weight: 3354
+};
