305d6f68c85d3d44a8f34c814d4d941021850194
Robin <daorob@gmail.com>
2020-01-09 00:33:03 -0600

    Add syntax around interfaces notes.

--- interfaces.ts
+++ interfaces.ts
@@ -1,14 +1,18 @@
 // a variable that refers to a type
 interface Vehicle {
     name: string;
-    year: number;
+    year: Date;
     broken: boolean;
+    summary(): string;
 }
 
 const oldCivic = {
     name: 'civic',
-    year: 2000,
-    broken: true
+    year: new Date(),
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
 };
 
 // to call this function, you must provide an object that has certain properties
@@ -16,6 +20,7 @@ const printVehicle = (vehicle: Vehicle): void => {
     console.log(`Name: ${vehicle.name}`);
     console.log(`Year: ${vehicle.year}`);
     console.log(`Broken: ${vehicle.broken}`);
+    console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
