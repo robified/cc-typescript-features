567f9bdadfb9d06e468dc3379e2bdc3ea788d35d
Robin <daorob@gmail.com>
2020-01-08 22:46:44 -0600

    Add anonymous function annotation notes.

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -5,3 +5,12 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number): number => {
     return a - b;
 };
+
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+// anonymouse function
+const multiple = function(a: number, b: number): number {
+    return a * b;
+};
