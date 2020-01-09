8af9f7b36abdca8129b19c3e75e2acf4087978f9
Robin <daorob@gmail.com>
2020-01-09 00:03:46 -0600

    Add tuples in action notes.

---
+++ tuples.ts
@@ -0,0 +1,19 @@
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+// const pepsi = ['brown', true, 40];
+// this will break the order and lose information
+// pepsi[0] = 40;
+// pepsi[2] = 'brown';
+
+// how to add a type alias
+type Drink = [string, boolean, number];
+
+// to add a tuple annotation
+const pepsi: Drink = ['brown', true, 40];
+pepsi[0] = 40; // won't work because we are assigning a different type
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
