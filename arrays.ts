122b117fd5c57135963d2182bf25815b9d2d97c0
Robin <daorob@gmail.com>
2020-01-08 23:34:33 -0600

    Add arrays TS notes.

---
+++ arrays.ts
@@ -0,0 +1,10 @@
+// this will return an any[]. We want to avoid this.
+// const carMakers = [];
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+
+// this will contain an array that contains instances of Dates
+const dates = [new Date(), new Date()];
+
+// const carsByMake: string[][] = [];
+// this is a 2 dimensional arrays
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
