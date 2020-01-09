b38f2ee1ea1a2e0c2afe36b98f5745119d9d7692
Robin <daorob@gmail.com>
2020-01-08 23:40:57 -0600

    Add why typped arrays notes.

--- arrays.ts
+++ arrays.ts
@@ -8,3 +8,16 @@ const dates = [new Date(), new Date()];
 // const carsByMake: string[][] = [];
 // this is a 2 dimensional arrays
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+    // we get access to a lot of auto complete methods from strings
+    return car.toUpperCase();
+});
