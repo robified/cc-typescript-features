ebe52c149ead296e43e59d99552d308cfea30795
Robin <daorob@gmail.com>
2020-01-08 22:55:58 -0600

    Add notes for void and never.

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -14,3 +14,15 @@ function divide(a: number, b: number): number {
 const multiple = function(a: number, b: number): number {
     return a * b;
 };
+
+const logger = (message: string): void => {
+    console.log(message);
+    // return null | undefined; // void can return null | undefined
+};
+
+const throwError = (message: string): void => {
+    // anytime you throw an error, the function will not return anything
+    if (!message) {
+        throw new Error(message);
+    }
+}; // because we're not returning anything and there's a chance of us never reaching the end of the function
